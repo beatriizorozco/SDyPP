f91d4c9f5f2ce2cc9953cdb88586fc17d9035099
beatriizorozco <beatrizorozcodelgado@gmail.com>
2025-09-24 09:47:59 +0200

    Commit 2

--- clase/ConsoleApp1/Program.cs
+++ clase/ConsoleApp1/Program.cs
@@ -1,7 +1,19 @@
-﻿internal class Program
+﻿using System;
+using ClassLibrary1;
+
+namespace ConsoleApp
 {
-    private static void Main(string[] args)
+    internal class Program
     {
-        Console.WriteLine("Hello, World!");
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            //Recoger un valor de la línea de comando
+            Datetime momentoActual = DateTime.Now;
+            string line = Console.ReadLine();
+            int elNumero = Convert.ToInt32(line);
+            ClassLibrary1.Persona laPersona = new ClassLibrary1.Persona("Marta");
+            Console.WriteLine(line.ToUpper(System.Globalization.CultureInfo.CurrentCulture));
+        }
     }
 }
---
+++ clase/ConsoleApp2/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ClassLibrary1
+{
+    public class Persona
+    {
+        public string Nombre = "Miguel";
+
+        public Persona()
+        {
+        }
+
+        public Persona(string nombre)
+        {
+            Nombre = nombre;
+        }
+    }
+}
