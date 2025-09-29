6d5f4b885f1097e987895e8b03decf380a4493be
beatriizorozco <beatrizorozcodelgado@gmail.com>
2025-09-29 08:57:37 +0200

    Clase vehiculo Mal

---
+++ CLASES/CLASE_2/ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace OO1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehiculo miVehiculo = new Vehiculo();
+            miVehiculo.Marca = "Seat";
+            Console.WriteLine(miVehiculo.Marca);
+        }
+    }
+}
---
+++ CLASES/CLASE_2/ConsoleApp1/Vehiculo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OO1
+{
+	internal class Vehiculo
+	{
+		internal string marca = "Ford";
+
+		public Vehiculo()
+		{
+
+		}
+
+		internal Vehiculo(string marca)
+		{
+			this.Marca = marca;
+		}
+
+		internal string Marca
+		{
+			get
+			{
+				return marca;
+			}
+			set
+			{
+				marca = value;
+			}
+		}
+	}
+}
