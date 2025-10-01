b53ce2b7d59508e887b18f13dc8f77ead9bea413
beatriizorozco <beatrizorozcodelgado@gmail.com>
2025-10-01 09:47:32 +0200

    Creo ejercicio 2

---
+++ CLASES/CLASE_3/Ejercicio2/Calculadora.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Ejercicio1
+{
+    internal class Calculadora
+    {
+        public int Sumar(int x, int y)
+        {
+            return x + y;
+        }
+
+        public int Restar(int x, int y)
+        {
+            return x - y;
+        }
+
+        public int Multiplicar(int x, int y)
+        {
+            return (x * y);
+        }
+
+        public int Dividir(int x, int y)
+        {
+            return (x / y);
+        }
+    }
+}
---
+++ CLASES/CLASE_3/Ejercicio2/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Ejercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculadora calculadora = new Calculadora();
+
+            Console.WriteLine("Escribe el primer número: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Escribe el segundo número: ");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"{x} + {y} = {calculadora.Sumar(x, y)}");
+            Console.WriteLine($"{x} - {y} = {calculadora.Restar(x, y)}");
+            Console.WriteLine($"{x} * {y} = {calculadora.Multiplicar(x, y)}");
+            if (y != 0)
+            {
+                Console.WriteLine($"{x} / {y} = {calculadora.Dividir(x, y)}");
+            } else
+            {
+                Console.WriteLine("No se puede realizar la división ya que no se puede dividir un número entre 0.");
+            }
+
+        }
+    }
+}
