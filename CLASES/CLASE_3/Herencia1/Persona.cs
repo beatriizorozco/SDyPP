cc2c5fbb85fb8b65c12a35e529c3e3f006f5e3e6
beatriizorozco <beatrizorozcodelgado@gmail.com>
2025-10-01 09:17:37 +0200

    Nombre

---
+++ CLASES/CLASE_3/Herencia1/Persona.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Herencia1
+{
+    public class Persona
+    {
+        public string Nombre { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string DNI { get; set; }
+
+        public Persona(String elNombre)
+        { 
+            this.Nombre = elNombre;
+        }
+        public void ComunicarIncidencia(string ExplicacionIncidencia)
+        {
+            Console.WriteLine($"Comunicar incidencia desde Persona: {this.Nombre}. Explicación: {ExplicacionIncidencia}");
+        }
+    }
+}
---
+++ CLASES/CLASE_3/Herencia1/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Herencia1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona persona = new Persona();
+            persona.Nombre = "Juan Pérez";
+            persona.DNI = "12345678A";
+            persona.ComunicarIncidencia("Estoy enfermo.");
+        }
+    }
+}
