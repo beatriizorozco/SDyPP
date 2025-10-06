afd3abedd5511a24cdf06c186cbd34483051fa98
beatriizorozco <beatrizorozcodelgado@gmail.com>
2025-10-06 09:00:08 +0200

    Ejercicio 4

---
+++ CLASES/CLASE_4/Primos/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Primos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                char opcion;
+
+                do
+                {
+                    Console.WriteLine("\n--- MENÚ PRINCIPAL ---");
+                    Console.WriteLine("P - Mostrar números primos (1 al 30)");
+                    Console.WriteLine("N - Mostrar números no primos (1 al 30)");
+                    Console.WriteLine("S - Salir del programa");
+                    Console.Write("Elige una opción: ");
+
+                    opcion = Char.ToUpper(Console.ReadKey().KeyChar);
+                    Console.WriteLine();
+
+                    switch (opcion)
+                    {
+                        case 'P':
+                            Console.WriteLine("\nMostrando números primos:\n");
+                            Varios.MostrarPrimos();
+                            break;
+
+                        case 'N':
+                            Console.WriteLine("\nMostrando números no primos:\n");
+                            Varios.MostrarNoPrimos();
+                            break;
+
+                        case 'S':
+                            Console.WriteLine("\nSaliendo del programa...");
+                            break;
+
+                        default:
+                            Console.WriteLine("\nOpción no válida. Por favor, pulsa P, N o S.");
+                            break;
+                    }
+
+                } while (opcion != 'S');
+            }
+        }
+    }
+}
---
+++ CLASES/CLASE_4/Primos/Varios.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Primos
+{
+    internal class Varios
+    {
+        public static void MostrarPrimos()
+        {
+            for (int i = 0; i <= 30; i++)
+            {
+                if (EsPrimo(i))
+                {
+                    Console.WriteLine($"El número {i} es primo.");
+                }
+            }
+        }
+        public static void MostrarNoPrimos()
+        {
+            for (int i = 0; i <= 30; i++)
+            {
+                if (!EsPrimo(i))
+                {
+                    Console.WriteLine($"El número {i} no es primo.");
+                }
+            }
+        }
+
+        private static bool EsPrimo(int numero)
+        { 
+           if (numero < 2)
+            {
+                return false;
+            }
+           else if (numero >= 2)
+            {
+                for (int i = 2; i <= Math.Sqrt(numero); i++)
+                {
+                    if (numero % i == 0)
+                        return false;
+                }
+            }
+            return true;
+        } 
+    }
+}
