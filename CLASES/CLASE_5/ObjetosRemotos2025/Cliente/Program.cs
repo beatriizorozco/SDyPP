0e124860264833b1beee780985c890c4eedaa970
beatriizorozco <beatrizorozcodelgado@gmail.com>
2025-10-27 09:24:42 +0100

    27/10/2025

---
+++ CLASES/CLASE_5/ObjetosRemotos2025/Cliente/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cliente
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpChannel tcpChannel = new TcpChannel();
+            ChannelServices.RegisterChannel(tcpChannel);
+            Type requiredType = typeof(MovieTicketInterface);
+            MovieTicketInterface remoteObject =
+            (MovieTicketInterface)Activator.GetObject(requiredType,
+            "tcp://localhost:9998/MovieTicketBooking");
+            Console.WriteLine(remoteObject.GetTicketStatus("Ticket No: 3344"));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CLASES/CLASE_5/ObjetosRemotos2025/Servidor/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+using System.Text;
+using System.Threading.Tasks;
+using MovieTicket;
+
+
+namespace Servidor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TicketServer();
+        }
+
+        static void TicketServer()
+        {
+            Console.WriteLine("Ticket Server started...");
+            TcpChannel tcpChannel = new TcpChannel(9998);
+            ChannelServices.RegisterChannel(tcpChannel);
+            Type commonInterfaceType = Type.GetType("Servidor.MovieTicket");
+            RemotingConfiguration.RegisterWellKnownServiceType(commonInterfaceType,
+            "MovieTicketBooking", WellKnownObjectMode.SingleCall);
+            System.Console.WriteLine("Press ENTER to quitnn");
+            System.Console.ReadLine();
+        }
+    }
+    public interface MovieTicketInterface
+    {
+        string GetTicketStatus(string stringToPrint);
+    }
+    public class MovieTicket : MarshalByRefObject, MovieTicketInterface
+    {
+        public string GetTicketStatus(string stringToPrint)
+        {
+            string returnStatus = "Ticket Confirmed";
+            Console.WriteLine("Enquiry for {0}", stringToPrint);
+            Console.WriteLine("Sending back status: {0}", returnStatus);
+            return returnStatus;
+        }
+    }
+}
